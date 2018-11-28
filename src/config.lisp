81cf77f8368cd40c48220cd13a18d95073088d15
Frederick Gibson <frederick@graphmetrix.com>
2018-11-28 10:28:08 -0800

    added ontologies

--- src/config.lisp
+++ src/config.lisp
@@ -59,6 +59,8 @@
 				 :owl "<http://www.w3.org/2002/07/owl#>"
 				 :st "<http://www.w3.org/ns/posix/stat#>"
 				 :gmx "<http://graphmetrix.com/node#>"
+				 :dul "<http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#>"
+				 :sio "<http://semanticscience.org/resource/>"
 				 )
 		 :ontology-ttl (:acl "http://www.w3.org/ns/auth/acl"
 				       :rdf "https://lov.linkeddata.es/dataset/lov/vocabs/rdf/versions/2014-02-25.n3"
@@ -77,6 +79,8 @@
 				       :owl "https://lov.linkeddata.es/dataset/lov/vocabs/owl/versions/2009-11-15.n3"
 				       :st "https://raw.githubusercontent.com/solid/vocab/master/posix-stat.rdf"
 				       :gmx "" ;;TODO add gmx via load file
+				       :dul "https://lov.linkeddata.es/dataset/lov/vocabs/dul/versions/2017-04-10.n3"
+				       :sio "https://lov.linkeddata.es/dataset/lov/vocabs/sio/versions/2016-09-27.n3"
 				       )
 		 :ontology-uri (:acl "http://www.w3.org/ns/auth/acl"
 				     :rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -95,6 +99,8 @@
 				     :owl "http://www.w3.org/2002/07/owl"
 				     :st "http://www.w3.org/ns/posix/stat"
 				     :gmx "http://graphmetrix.com/node#7"
+				     :dul "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl"
+				     :sio "http://semanticscience.org/ontology/sio.owl"
 				     )
 				    
 				      
